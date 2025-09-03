1af66394e8e8505a2cc9e0dbf85062626f518a72
jao1311 <145819511+jao1311@users.noreply.github.com>
2025-09-03 16:33:41 -0300

    🙋‍♂️

--- src/controllers/Material.js
+++ src/controllers/Material.js
@@ -1,9 +1,23 @@
 import prisma from '../prisma';
+export const MaterialController = {
+    async store(req, res, next){
+        try{
 
-async store(req, res, next){
-    const {name, type, pollutionLevel, weight, rewardPoints, amount} = req.body;
-    
-    prisma.material.create(
-        data : {name, type, pollutionLevel, weight, rewardPoints, amount}
-    );
+            const {name, type, pollutionLevel, weight, rewardPoints, amount} = req.body;
+            
+            const m = await prisma.material.create({
+                data : {
+                    name,
+                    type,
+                    pollutionLevel,
+                    weight,
+                    rewardPoints,
+                    amount
+                }
+            });
+            res.status(201).json(m);
+        }catch(err){
+            next(err);
+        }
+    }
 }
