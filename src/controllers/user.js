3494552ff77910dd0d46fec4e40643d072ccfe44
rafaela-saraiva <rafaela.saraiva100@hotmail.com>
2025-09-03 16:34:32 -0300

    comit

---
+++ src/controllers/user.js
@@ -0,0 +1,27 @@
+import prisma from '../prisma';
+
+//nome da funcao (recebendo,responder,proximo)
+export const UserControler= {
+    async store(req, res, next){
+        try{
+            const {id,  email, pass, name,  cpf, phone } = req.body;
+
+            const u = await prisma.user.create({
+                data: { 
+                    id,  
+                    email, 
+                    pass,
+                    name,   
+                    cpf, 
+                    phone
+
+                }
+            });
+          
+            res.status(201).json(u);
+        }catch(err){
+            next(err);
+        }
+    }
+
+}
