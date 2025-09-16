629a3efb15445c0e6f7c7da9b5a38cad3023dff9
rafaela-saraiva <rafaela.saraiva100@hotmail.com>
2025-09-16 14:56:07 -0300

    comit

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,4 +4,6 @@ import { UserController } from '../controllers/user.js';
 const route = Router();
 
 route.post('/', UserController.store);
+route.get('/',UserController.index);
+
 export default route;
