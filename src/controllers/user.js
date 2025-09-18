8715749292641633bf17123c1a17141ced45eca2
rafaela-saraiva <rafaela.saraiva100@hotmail.com>
2025-09-18 15:11:39 -0300

    put feito

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -24,10 +24,12 @@ export const UserController= {
         }
     },
     async index(req, res, next){
+        let query = {}
+
+        if (req.query.name) query.name = {contains: req.query.name}
+
         const users = await prisma.user.findMany({
-            where:{
-                name: req.query.name,
-            }
+            where: query 
         })
 
         res.status(200).json(users)
@@ -55,5 +57,28 @@ export const UserController= {
         }catch(err){
             res.status(404).json({error:"Não encontrado"});
         }
-    }
-}
+    },
+
+
+    async upd(req, res, _next){
+        try{
+            const id = Number( req.params.id)
+
+            let body = {};
+
+            if (req.body.name) body.name = req.body.name
+            if (req.body.email) body.email = req.body.email
+            if (req.body.cpf) body.cpf = req.body.cpf
+            if (req.body.phone) body.phone = req.body.phone
+
+            const u = await prisma.user.update({
+                where: { id },
+                data: body
+            });
+
+            res.status(200).json(u)
+        }catch(err){
+            res.status(404).json({error:"Não encontrado"});
+        }
+    },
+}            
