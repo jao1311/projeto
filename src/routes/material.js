1e5ccd8a9ecc065d860f2e16cb047e2fe7d2aa0d
jao1311 <145819511+jao1311@users.noreply.github.com>
2025-09-18 14:40:22 -0300

    a

--- src/routes/material.js
+++ src/routes/material.js
@@ -6,5 +6,6 @@ const route = Router( );
 route.post('/', MaterialController.store);
 route.get('/:id', MaterialController.show);
 route.delete('/:id', MaterialController.del);
+route.put('/:id', MaterialController.upd);
 route.get('/', MaterialController.index);
 export default route;
