917b6af42e55544905bdb9cbaa419d80f234fc2e
giovanna2703 <gigisouza628@gmail.com>
2025-09-18 13:37:26 -0300

    atualização

--- src/routes/enterprise.js
+++ src/routes/enterprise.js
@@ -4,6 +4,9 @@ import { EnterpriseController } from '../controllers/enterprise.js';
 const route = Router();
 
 route.post('/', EnterpriseController.store);
+route.get('/:id', EnterpriseController.show);
+route.delete('/:id', EnterpriseController.del);
 route.get('/', EnterpriseController.index);
+
 
 export default route;
